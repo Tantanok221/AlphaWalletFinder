c7cfdfdee763a148c38bcc0caac06eaec579ba54
Stangen <stangendeveloper@gmail.com>
2025-07-19 21:58:14 +0800

    test: fix main test file placeholder

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test("basic test placeholder", () => {
+  assertEquals(1 + 1, 2);
 });
