df7c1db2406de02ff89ec635066ff802aad86208
Stangen <stangendeveloper@gmail.com>
2025-07-19 21:57:34 +0800

    fix: add redis cleanup to prevent process hanging

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import {getTokenHolderWithinDateRange} from "./use_case/getTokenHolderWithinDateRange.ts";
+import { closeRedis } from "./helper/redis.ts";
 
-await getTokenHolderWithinDateRange({
-  tokenAddress: "4ZVCJdxrbWLWKFFAZqqphyNBUhPE7YpnHWXvFuKkbonk",
-  endDate: new Date("2025-07-19T07:00:00.000Z"),
-})
+try {
+  await getTokenHolderWithinDateRange({
+    tokenAddress: "4ZVCJdxrbWLWKFFAZqqphyNBUhPE7YpnHWXvFuKkbonk",
+    endDate: new Date("2025-07-19T07:00:00.000Z"),
+  });
+} finally {
+  await closeRedis();
+}
